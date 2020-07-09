0e167f27d4489a81b4dfaef322859de4b662761f
Daffy Chu <danielchu0710@hotmail.com>
2020-07-09 09:16:23 -0400

    Fix Incorrect parsing and db sending error
    Former-commit-id: a62e40b3632d629fd8ec21a5fab004d1dc057921

--- db/languageSource.sql
+++ db/languageSource.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS languageSource (
     id serial not NULL,
     sense_id int REFERENCES sense (id),
-    lang varchar(50) not NULL,
-    wasei boolean not NULL DEFAULT False,
-    text text not NULL,
+    lang varchar(50) NULL,
+    wasei boolean NULL DEFAULT False,
+    text text NULL,
     PRIMARY KEY (id, sense_id) 
 )
--- db/partsOfSpeech.sql
+++ db/partsOfSpeech.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS partsOfSpeech (
     id serial not NULL,
     sense_id int REFERENCES sense (id),
-    expl text not NULL,
+    expl text NULL,
     PRIMARY KEY (id, sense_id) 
 )
