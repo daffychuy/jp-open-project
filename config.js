58532a395bcf2f09eb6bbdd52fc1b00f5e717a50
Daffychuy <danielchu0710@hotmail.com>
2020-09-15 13:22:45 -0400

    Update code to sync with remote

---
+++ config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+// pg-promise for our database handler
+// Which is derived from pg
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    client_encoding: 'utf-8',
+});
+
+module.exports = pool;
