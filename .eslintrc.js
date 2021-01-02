efe1129e933a9faad77ada19bba8e825b31ca036
daffychuy <danielchu0710@hotmail.com>
2021-01-02 12:42:55 -0500

    Add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    // "commonjs": true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    // "plugin:react/recommended"
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  // "plugins": [
-  // 	"react"
-  // ],
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+	env: {
+		browser: true,
+		// "commonjs": true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		// "plugin:react/recommended"
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	// "plugins": [
+	// 	"react"
+	// ],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
 };
