8ccc63391a65a0467c91cfe458f53d6b915b7390
daffychuy <danielchu0710@hotmail.com>
2021-06-21 15:37:33 +0800

    Sync code with remote

--- database/search.go
+++ database/search.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/daffychuy/jp-open-project/middleware"
@@ -9,16 +10,23 @@ import (
 )
 
 func GetWords(word string) {
-	cur, err := middleware.Collection.Find(context.Background(), bson.D{{}})
+
+	cur, err := middleware.Collection.Find(
+		context.Background(),
+		bson.M{"Japanese.kana": word})
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for cur.Next(context.Background()) {
 		var result bson.M
 		e := cur.Decode(&result)
 		if e != nil {
 			log.Fatal(e)
 		}
+		fmt.Println(e)
+		fmt.Println(result)
 
 	}
 }
--- models/models.go
+++ models/models.go
@@ -5,8 +5,41 @@ import (
 )
 
 type JMDict struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	JMdict_id string             `json:"JMdict_id,omitempty"`
-	Japanese  []string           `json:"Japanese,omitempty"`
-	Sense     []string           `json:"sense,omitempty"`
+	ID        primitive.ObjectID `json:"_id" bson:"_id"`
+	JMdict_id string             `json:"JMdict_id"`
+	Japanese  []Reading          `json:"Japanese"`
+	Sense     []string           `json:"sense"`
+}
+
+type Reading struct {
+	Kanji        string `json:"kanji,omitempty"`
+	Kana         string `json:"kana,omitempty"`
+	Kanji_Common string `json:"kanji_common,omitempty"`
+	Kana_Common  string `kson:"kana_common,omitempty"`
+}
+
+type Sense struct {
+	PartsOfSpeech  []string     `json:"partsOfSpeech"`
+	AppliesToKanji []string     `json:"appliesToKanji"`
+	AppliesToKana  []string     `json:"appliesToKana"`
+	Related        []string     `json:"related"`
+	Antonym        []string     `json:"antonym"`
+	Field          []string     `json:"field"`
+	Dialect        []string     `json:"dialect"`
+	Misc           []string     `json:"misc"`
+	Info           []string     `json:"info"`
+	LanguageSource []LangSource `json:"languageSource"`
+	Gloss          []gloss      `json:"gloss"`
+}
+
+type LangSource struct {
+	Lang  string `json:"lang"`
+	Wasei bool   `json:"wasei"`
+	Text  string `json:"text"`
+}
+
+type gloss struct {
+	Lang string `json:"lang"`
+	Type string `json:"type"`
+	Text string `json:"text"`
 }
